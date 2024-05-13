42f419df66f981b442babb7bce286641f050f27e
rkdbs <w2207@e-mirim.hs.kr>
2024-05-13 12:16:36 +0900

    views CharacterListView

--- NetworkProgramming_helloidol2/브레드이발소/views.py
+++ NetworkProgramming_helloidol2/브레드이발소/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from 브레드이발소.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # character_list = Character.objects.all() # DB에 Character 데이터 다 가져오자
+    # return render(request, '브레드이발소/character_list.html', context={'character_list': character_list}) # 모델_list.html에 모델_list라는 키로 DB에 가져온 데이터 넣어서 render 하자
