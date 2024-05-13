7bf3d1f44dc71e6cd88839bfa3393cfbbbd1ce4c
rkdbs <w2207@e-mirim.hs.kr>
2024-05-13 11:47:55 +0900

    models 브레드이발소, makemigrations, migrate

--- NetworkProgramming_helloidol2/README.md
+++ NetworkProgramming_helloidol2/README.md
@@ -11,3 +11,9 @@
 2. startapp 브레드이발소
    1. python manage.py startapp 브레드이발소
    2. '브레드이발소', in INSTALLED_APPS in settings.py
+3. 브레드이발소/
+   1. models
+      1. Character
+         1. name, feature, created_at, updated_at
+      2. python manage.py makemigrations 브레드이발소
+      3. python manage.py migrate 브레드이발소
--- NetworkProgramming_helloidol2/브레드이발소/models.py
+++ NetworkProgramming_helloidol2/브레드이발소/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=20) # max_length 필수
+    feature = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
